8e9831bd501d86d04bb6da59bc556702d70d24ff
mixxue <pison.mick1@gmail.com>
2026-01-13 13:35:47 +0700

    Update lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for(int i=1; i<N; i++){
+		cout << "Pass " << i << ":";
+		T key = d[i];
+		int j = i-1;
+		while (j >= 0 && d[j] < key)
+		{
+			d[j+1] = d[j];
+			j = j-1;
+		}
+		d[j+1] = key;
+	
+	for(int j=0;j < N;j++){  
+			cout << d[j] ; 
+			if(j!=N-1) cout << " "; 
+		}
+		cout << endl; 
+	}
 }
 
 int main(){
